24e6d1f79e5a4a72e47c6c62b0aa494f95bbce64
Sceriffo123 <morana.marco@gmail.com>
2025-09-04 13:07:02 +0200

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1921,7 +1921,7 @@ export default function Admin() {
             )}
           </div>
         </div>
-        
+
         {/* Modal Aggiungi Utente */}
         {showAddUser && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
@@ -3887,9 +3887,9 @@ export default function Admin() {
           </div>
         </div>
       </div>
-    )}
-    </div>
-    </div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
